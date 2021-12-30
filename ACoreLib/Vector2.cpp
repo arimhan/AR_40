edd153d8088cd667002a7decbf3e6cec9cb67707
arimhan <arhan1015@gmail.com>
2021-12-30 10:12:51 +0900

    211230...

--- ACoreLib/Vector2.cpp
+++ ACoreLib/Vector2.cpp
@@ -43,7 +43,6 @@ AVector2 AVector2::operator /(float fValue)
 	return *this;
 	//return AVector2(fValue / x, fValue / y);
 }
-<<<<<<< HEAD
 float AVector2::Length() //제곱에 루트씌워서 거리계산
 {
 	float ret = x * x + y * y;
@@ -60,18 +59,12 @@ AVector2 AVector2::Normal() //자기자신만 정규화
 {
 	AVector2 ret;
 	float normal = 1.0f / Length();
-	ret.x = x * normal;
-	ret.y = y * normal;
-	return ret;
-	//return AVector2 (ret.x = x * normal,  ret.y = y * normal);
+	return AVector2 (ret.x = x * normal,  ret.y = y * normal);
 }
 AVector2 AVector2::Normalization()
 {
 	float normal = 1.0f / Length();
-	x *= normal;
-	y *= normal;
-	return *this;
-	//return AVector2( x *= normal, y *= normal);
+	return AVector2( x *= normal, y *= normal);
 }
 AVector2 Normalization(AVector2& v) //다른 기능의 정규화 추가 (v를 받아 정규화 처리)
 {
@@ -79,14 +72,11 @@ AVector2 Normalization(AVector2& v) //다른 기능의 정규화 추가 (v를 받아 정규화 �
 	float normal = 1.0f / v.Length();
 	return AVector2( ret.x = v.x* normal, ret.y = v.y*normal );
 }
-bool AVector2::operator ==(const AVector2& v)  
-=======
-AVector2 AVector2::operator /(float fValue)
+AVector2 AVector2::operator /(float fValue) 
 {
 	return AVector2(fValue / x, fValue / y);
 }
-bool AVector2::operator ==(const AVector2& v)
->>>>>>> 75e3da97d6de4e92a326993dfda6eb745a0d22b7
+bool AVector2::operator ==(const AVector2& v)  
 // 기존 값 - obj의 x,y값의 차가 0에 가까우면 (실수니까 오차범위땜시 fabs와 0.0001f 로 비교) 같기 때문에 true 반환
 {
 	if (fabs(x - v.x) < 0.0001f)
